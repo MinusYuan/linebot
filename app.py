dd1e9c76e92b37d3ee8dfa0b77d0a61363ec5fe2
minusyuan <rod92540@gmail.com>
2023-11-24 17:04:06 +0800

    Show message

--- app.py
+++ app.py
@@ -91,7 +91,7 @@ def message_text(event):
     reply = con.console(user_id, mess)
     if not reply:
         return
-    print(f"UID: {user_id}, Reply: {reply[:15]}")
+    print(f"UID: {user_id}, Reply: {reply.split('\n')[0]}")
     with ApiClient(configuration) as api_client:
         line_bot_api = MessagingApi(api_client)
         profile = line_bot_api.get_profile(user_id)
