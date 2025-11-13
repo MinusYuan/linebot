c83afdd6070d216a021b6d04a4d9684f53edbee0
auggiejhang <rod92540@gmail.com>
2025-11-13 11:28:44 +0800

    Fix

--- console.py
+++ console.py
@@ -242,7 +242,7 @@ RM <手機號碼> \n    -> (移除現有手機號碼綁定)
 
             count = 0
             for key, stock_code in stock_key_mapping:
-                num = int(d[key])
+                num = int(d.get(key, 0))
                 if num <= 0:
                     continue
 
